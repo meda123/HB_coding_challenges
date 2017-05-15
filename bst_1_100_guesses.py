4b00d7ab94a218e1e7baa968fc5d9f1ceb637329
meda123 <mrt62@cornell.edu>
2017-05-14 17:52:26 -0700

    initial bst 1_100 commit, added.

---
+++ bst_1_100_guesses.py
@@ -0,0 +1,45 @@
+
+"""
+Binary search is one of the most important Computer Science algorithms. 
+It allows you to search a sorted list in O(log n) time, a large improvement 
+over scanning every item in the list (which would be O(n) time).
+
+In this challenge, you’ll make binary search for the classic children’s guessing
+game of “pick a number from 1 to 100”.
+
+"""
+
+def binary_search(val):
+    """
+    Function returns number of guesses
+
+    >>> binary_search(50)
+    1
+
+    >>> binary_search(25)
+    2
+
+    >>> binary_search(75)
+    2
+
+    >>> binary_search(31) <= 7
+    True
+
+    >>> max([binary_search(i) for i in range(1, 101)])
+    7 
+
+    """
+
+
+
+##################### DocTests ###########################################
+
+if __name__ == "__main__":
+    import doctest
+
+    print
+    result = doctest.testmod()
+    if not result.failed:
+        print "ALL TESTS PASSED. GOOD WORK!"
+    print
+
