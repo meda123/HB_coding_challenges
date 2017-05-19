aca0cb70d12d4b775b0553539e4c08b2e0fae1f7
meda123 <mrt62@cornell.edu>
2017-05-18 22:37:07 -0700

    Initial commit.

---
+++ count_recursively.py
@@ -0,0 +1,13 @@
+"""
+Count the number of items in a list, using recursion.
+
+"""
+
+def count_recursively(items):
+"""
+    >>> count_recursively([])
+    0
+    >>> count_recursively([1, 2, 3])
+    3
+"""
+
