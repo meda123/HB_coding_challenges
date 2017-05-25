c7ba7ca0e91efcff8969f264b98309aa2d3613c5
meda123 <mrt62@cornell.edu>
2017-05-24 22:57:37 -0400

    Initial commit.

---
+++ hybrid_reg.py
@@ -0,0 +1,40 @@
+
+"""
+This calculator helps to calculate the benefits of purchasing a Hybrid vehicle 
+versus a gasoline vehicle. A basic program written for and with my brother, a 
+high school student who knew very little about programming.
+
+"""
+
+print "Greetings! Welcome to the vehicle purchase calculator."
+
+# Assumed variables  
+tax_credit = 2,000
+gas_per_mile = 2.5
+average_vehicle_lifetime = 10 
+
+# Ask user input for mileage 
+hybrid_milage = 
+hybrid_cost = 
+
+# Ask user input for mileage  
+regular_milage = 
+regular_cost = 
+
+# Ask user input, "On average how many miles do you driver per year"
+miles_per_year = 
+
+cost_difference = hybrid_cost - regular_cost
+years_until_benefit = 
+
+
+
+print "The cost difference is: ___"
+print "This year, the cost difference is: ___"
+print "It will take approximately ___ years to reap the benefits of a hybrid"
+
+print "In a vehicle's lifetime, it will take approximately "
+
+
+
+
