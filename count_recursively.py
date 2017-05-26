8212e0884ef3f6868c3ecaff9bc406b4c6d4d0a7
meda123 <mrt62@cornell.edu>
2017-05-25 23:25:58 -0400

    Challenge completed.

--- count_recursively.py
+++ count_recursively.py
@@ -4,10 +4,22 @@ Count the number of items in a list, using recursion.
 """
 
 def count_recursively(items):
-"""
+    """
     >>> count_recursively([])
     0
     >>> count_recursively([1, 2, 3])
     3
-"""
+    """
+
+    if not items: 
+        return 0
+
+    return 1 + count_recursively(items[1:])
+
+if __name__ == '__main__':
+    import doctest 
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TEST PASSED. DANCE IT OUT!"
+
+
 
