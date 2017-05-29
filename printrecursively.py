4059d105e12d8e25e414fd3f10d670b739d8b74a
meda123 <mrt62@cornell.edu>
2017-05-29 00:03:20 -0400

    Initial commit.

--- printrecursively.py
+++ printrecursively.py
@@ -12,10 +12,6 @@ For example::
 
 def print_recursively(lst):
     """Print items in the list, using recursion."""
-    if lst: 
-        print lst[0]
-        print_recursively(lst[1:])
-
 
 if __name__ == '__main__':
     import doctest
