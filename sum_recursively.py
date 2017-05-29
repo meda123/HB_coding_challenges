8a8c613fb39b21e2e9fc7b7141f0f74a568c13d6
meda123 <mrt62@cornell.edu>
2017-05-29 00:38:12 -0400

    challenge completed.

--- sum_recursively.py
+++ sum_recursively.py
@@ -23,6 +23,10 @@ The sum of an empty list is zero::
 
 def sum_list(nums):
     """Using recursion, return the sum of numbers in a list."""
+    if not nums:
+        return 0 
+
+    return nums[0] + sum_list(nums[1:])
 
 
 if __name__ == '__main__':
